42fe401834af1fed6cb87d80674cc4087614272f
Harold Angenent <harold@trendwerk.nl>
2017-09-19 22:01:44 +0200

    Add player

---
+++ src/player.js
@@ -0,0 +1,59 @@
+import * as firebase from 'firebase'
+import randomEmoji from 'random-emoji'
+
+export default function createPlayer(world, keyHandler) {
+  const db = firebase.database()
+
+  const emoji = randomEmoji.random({ count: 1 }).shift()
+
+  const userId = db.ref('users').push().key
+  db.ref(`users/${userId}`).update({ name: emoji.name, char: emoji.character, score: 0 })
+
+  let isJumping = true
+  const size = 30
+  const speed = 0.5
+  const movement = { x: 0, y: 0 }
+  const position = { x: 0, y: size }
+
+  return {
+    draw() {
+      world.getContext().font = `${size}px sans-serif`
+      world.getContext().fillText(emoji.character, position.x, position.y)
+    },
+    update(delta) {
+      movement.x = 0
+      movement.y += (delta / 30)
+
+      // Horizontal movement
+      if (keyHandler.isDown('ArrowRight')) {
+        movement.x = speed * delta
+      } else if (keyHandler.isDown('ArrowLeft')) {
+        movement.x = -speed * delta
+      }
+
+      // Jump
+      if (keyHandler.isDown(' ') && !isJumping) {
+        isJumping = true
+        movement.y = -15
+      }
+
+      // Move
+      position.x += movement.x
+      position.y += movement.y
+
+      // Boundaries
+      if (position.y >= world.getHeight()) {
+        isJumping = false
+        position.y = world.getHeight()
+      }
+
+      if (position.x <= 0) {
+        position.x = 0
+      }
+
+      if (position.x >= (world.getWidth() - size)) {
+        position.x = (world.getWidth() - size)
+      }
+    }
+  }
+}
