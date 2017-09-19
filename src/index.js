a48b5f1e0937b94118515545375282a15ae60f9b
Harold Angenent <harold@trendwerk.nl>
2017-09-19 22:02:17 +0200

    Initialize, create game loop

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase'
+import createPlayer from './player'
+import createWorld from './world'
+import createKeyHandler from './keyHandler'
+import env from '../env'
+
+firebase.initializeApp(env.firebase)
+
+const keyHandler = createKeyHandler()
+const world = createWorld()
+const player = createPlayer(world, keyHandler)
+
+let lastUpdate;
+
+const loop = () => {
+  const now = Date.now()
+  const delta = now - lastUpdate
+
+  world.reset()
+
+  player.update(delta)
+  player.draw()
+
+  lastUpdate = now
+  requestAnimationFrame(loop)
+}
+
+lastUpdate = Date.now()
+requestAnimationFrame(loop)
